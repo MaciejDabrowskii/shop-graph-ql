72ca039504879dc4577607be9bbbfd8140160d02
Maciej Dąbrowski <dabrowski.mac.1@gmail.com>
2022-08-29 16:13:18 +0200

    currency selector

--- src/components/navbar/components/currency_selector.js
+++ src/components/navbar/components/currency_selector.js
@@ -6,20 +6,32 @@ import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 
 const GET_CURRENCY = gql`
-query{currencies{label symbol}}`;
+query{
+    currencies{
+        label
+        symbol
+    }
+}`;
 
 function CurrencySelector()
 {
-  const [currencies, setCurrencies] = useState(null);
-  const { data, error, loading } = useQuery(GET_CURRENCY);
+  const [currencies, setCurrencies] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState("");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { data, error, loading } = useQuery(GET_CURRENCY, { fetchPolicy: "cache-and-network" });
 
   useEffect(() =>
   {
-    setCurrencies(data);
-  }, [data]);
-
-  console.log(data);
-  //   const [isMenuOpen, setIsMenuOpen] = useState(false);
+    if (!loading)
+    {
+      setCurrencies(data.currencies);
+      console.log(currencies);
+      currencies.map((currency) =>
+      {
+        if (currency.label === "USD")setSelectedCurrency(currency.label);
+      });
+    }
+  }, [loading]);
 
   if (error) return console.log(error);
   if (loading) return <h1>Loading...</h1>;
@@ -28,12 +40,13 @@ function CurrencySelector()
 
     <div className="currency-selector-container">
       <div className="currency-selector-symbol">
-        {data.currencies.map((currency) =>
+        {currencies.map((currency) =>
         {
           if (currency.label === "USD") return <p>{currency.symbol}</p>;
         })}
       </div>
-      <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <div>{selectedCurrency}</div>
+      <svg className="currency-selector-arrow" width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 3.5L4 0.5L7 3.5" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
 
@@ -41,4 +54,8 @@ function CurrencySelector()
   );
 }
 
+// {data.currencies.map((currency) =>
+// {
+//   if (currency.label === "USD") return <p>{currency.symbol}</p>;
+// })}
 export default CurrencySelector;
