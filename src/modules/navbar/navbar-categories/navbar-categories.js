7da32d89abc15a0c48cfd4aa1ac509374ef89e6d
Maciej Dąbrowski <dabrowski.mac.1@gmail.com>
2022-10-10 15:21:21 +0200

    html and css tweaks

--- src/modules/navbar/navbar-categories/navbar-categories.js
+++ src/modules/navbar/navbar-categories/navbar-categories.js
@@ -44,15 +44,11 @@ function NavbarCategories({ setSelectedCategory, selectedCategory })
         <Link
           to="/"
           key={name}
+          onClick={() => setSelectedCategory(name)}
+          className={`navbar-category${
+            selectedCategory === name ? " active" : ""}`}
         >
-          <button
-            type="button"
-            onClick={() => setSelectedCategory(name)}
-            className={`navbar-category${
-              selectedCategory === name ? " active" : ""}`}
-          >
-            {name.toUpperCase()}
-          </button>
+          {name.toUpperCase()}
         </Link>
       ))}
     </div>
