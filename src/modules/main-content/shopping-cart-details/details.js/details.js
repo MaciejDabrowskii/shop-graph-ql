e4eab038c04235d0365b87556c2ed86216dd8de6
Maciej Dąbrowski <dabrowski.mac.1@gmail.com>
2022-10-13 10:21:02 +0200

    Changed function to the class

--- src/modules/main-content/shopping-cart-details/details.js/details.js
+++ src/modules/main-content/shopping-cart-details/details.js/details.js
@@ -1,39 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   calculateSum,
   calculateTax,
   calculateCartItemsQuantity,
 } from "../../../shopping-cart-functions/shopping-cart-functions";
 
-function Details({ shoppingCartItems, selectedCurrency })
+class Details extends Component
 {
-  return (
-    <div className="shoppingCartDetails-details-container">
-      <p>
-        Tax 21%:
-        {" "}
-        <span className="bolder">
-          {selectedCurrency.symbol}
-          {calculateTax(calculateSum, shoppingCartItems, selectedCurrency)}
-        </span>
-      </p>
-      <p>
-        Quantity:
-        {" "}
-        <span className="bolder">
-          {calculateCartItemsQuantity(shoppingCartItems)}
-        </span>
-      </p>
-      <p className="total">
-        Total:
-        {" "}
-        <span className="bolder">
-          {selectedCurrency.symbol}
-          {calculateSum(shoppingCartItems, selectedCurrency)}
-        </span>
-      </p>
-    </div>
-  );
+  constructor(props)
+  {
+    super(props);
+  }
+
+  render()
+  {
+    const {
+      shoppingCartItems,
+      selectedCurrency,
+    } = this.context;
+
+    return (
+      <div className="shoppingCartDetails-details-container">
+        <p>
+          Tax 21%:
+          {" "}
+          <span className="bolder">
+            {selectedCurrency.symbol}
+            {calculateTax(calculateSum, shoppingCartItems, selectedCurrency)}
+          </span>
+        </p>
+        <p>
+          Quantity:
+          {" "}
+          <span className="bolder">
+            {calculateCartItemsQuantity(shoppingCartItems)}
+          </span>
+        </p>
+        <p className="total">
+          Total:
+          {" "}
+          <span className="bolder">
+            {selectedCurrency.symbol}
+            {calculateSum(shoppingCartItems, selectedCurrency)}
+          </span>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Details;
