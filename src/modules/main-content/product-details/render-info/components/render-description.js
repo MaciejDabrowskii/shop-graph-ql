da91940ce5dcd89d1d5642143fd5bdc9b38d9a70
Maciej Dąbrowski <dabrowski.mac.1@gmail.com>
2022-10-25 01:05:23 +0200

    added interweave to handle InnerHTML

--- src/modules/main-content/product-details/render-info/components/render-description.js
+++ src/modules/main-content/product-details/render-info/components/render-description.js
@@ -1,15 +1,18 @@
 /* eslint-disable react/no-danger */
 
 import React from "react";
-import DOMPurify from "dompurify";
+// import DOMPurify from "dompurify";
+import { Markup } from "interweave";
 
 function RenderDescription({ description })
 {
   return (
     <div className="product-details-info-description-container">
-      <div
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
-      />
+      {/* <div
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }} // sanitize approach
+      /> */}
+      <Markup content={description} />
+      {/* using interweave approach */}
     </div>
   );
 }
